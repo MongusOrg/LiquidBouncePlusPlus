d140df18ffd8d34e1e8a8644444beb0e0bec9b36
exit-scammed <85971344+exit-scammed@users.noreply.github.com>
2021-12-26 18:28:36 +0700

    hit

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ItemPhysics.java
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/ItemPhysics.java
@@ -8,7 +8,7 @@ package net.ccbluex.liquidbounce.features.module.modules.render;
 import net.ccbluex.liquidbounce.features.module.*;
 import net.ccbluex.liquidbounce.value.FloatValue;
 
-@ModuleInfo(name = "ItemPhysics", spacedName = "Item Physics", description = "newton in action", category = ModuleCategory.RENDER)
+@ModuleInfo(name = "ItemPhysics", spacedName = "Item Physics", description = "newton hits", category = ModuleCategory.RENDER)
 public class ItemPhysics extends Module
 {
     public final FloatValue itemWeight = new FloatValue("Weight", 0.5F, 0F, 1F);
