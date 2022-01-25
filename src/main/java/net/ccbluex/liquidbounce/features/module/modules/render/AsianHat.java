3036c013a4d75dd446b1945e6f5396397636aed3
exit-scammed <85971344+exit-scammed@users.noreply.github.com>
2022-01-25 18:11:28 +0700

    forgot the import

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/AsianHat.java
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/AsianHat.java
@@ -16,6 +16,7 @@ import net.ccbluex.liquidbounce.features.module.Module;
 import net.ccbluex.liquidbounce.features.module.ModuleCategory;
 import net.ccbluex.liquidbounce.features.module.ModuleInfo;
 import net.ccbluex.liquidbounce.ui.font.GameFontRenderer;
+import net.ccbluex.liquidbounce.utils.RotationUtils;
 import net.ccbluex.liquidbounce.utils.render.ColorUtils;
 import net.ccbluex.liquidbounce.utils.render.RenderUtils;
 import net.ccbluex.liquidbounce.value.*;
