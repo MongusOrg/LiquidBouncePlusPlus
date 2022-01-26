9354ce1db3ad7e32f7c8a1e434bbd2a0770a0cf8
exit-scammed <85971344+exit-scammed@users.noreply.github.com>
2022-01-26 20:31:26 +0700

    am i done

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/network/MixinNetworkManager.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/network/MixinNetworkManager.java
@@ -16,6 +16,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(NetworkManager.class)
 public class MixinNetworkManager {
