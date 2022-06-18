45fd7f8017ac014a5eccbbbd57c09a020677dfcb
exit-scammed <bachnguyenkim2007@gmail.com>
2022-06-19 06:11:04 +0700

    negative

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/AsianHat.java
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/AsianHat.java
@@ -102,8 +102,8 @@ public class AsianHat extends Module {
         pre3D();
         
         if (hatRotation.get() && RotationUtils.serverRotation != null) {
-            GlStateManager.rotate(RotationUtils.serverRotation.getYaw(), 0, 1, 0);
-            GlStateManager.rotate(RotationUtils.serverRotation.getPitch(), 1, 0, 0);
+            GlStateManager.rotate(-RotationUtils.serverRotation.getYaw(), 0, 1, 0);
+            GlStateManager.rotate(-RotationUtils.serverRotation.getPitch(), 1, 0, 0);
         }
 
         worldrenderer.begin(GL11.GL_POLYGON, DefaultVertexFormats.POSITION_COLOR);
