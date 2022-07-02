e5b0f00b082913160935e2774dcd0a30f6649c5f
exit-scammed <bachnguyenkim2007@gmail.com>
2022-07-02 18:18:16 +0700

    idk

--- src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/targets/impl/Exhibition.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/hud/element/elements/targets/impl/Exhibition.kt
@@ -23,7 +23,7 @@ class Exhibition(inst: Target): TargetStyle("Exhibition", inst, false) {
 
     override fun drawTarget(entity: EntityPlayer) {
         val font = Fonts.fontTahoma
-        val minWidth = 136F.coerceAtLeast(50F + font.getStringWidth(entity.name))
+        val minWidth = 126F.coerceAtLeast(40F + font.getStringWidth(entity.name))
 
         RenderUtils.drawExhiRect(0F, 0F, minWidth, 45F, 1F - targetInstance.getFadeProgress())
 
@@ -31,18 +31,18 @@ class Exhibition(inst: Target): TargetStyle("Exhibition", inst, false) {
         RenderUtils.drawRect(3F, 3F, 42F, 42F, getColor(Color(19, 19, 19)).rgb)
 
         GL11.glColor4f(1f, 1f, 1f, 1f - targetInstance.getFadeProgress())
-        RenderUtils.drawEntityOnScreen(22, 40, 15, entity)
+        RenderUtils.drawEntityOnScreen(22, 40, 16, entity)
 
         font.drawString(entity.name, 46, 5, getColor(-1).rgb)
 
         val barLength = 70F * (entity.health / entity.maxHealth).coerceIn(0F, 1F)
-        RenderUtils.drawRect(45F, 14F, 45F + 70F, 18F, getColor(BlendUtils.getHealthColor(entity.health, entity.maxHealth).darker(0.4F)).rgb)
+        RenderUtils.drawRect(45F, 14F, 45F + 70F, 18F, getColor(BlendUtils.getHealthColor(entity.health, entity.maxHealth).darker(0.3F)).rgb)
         RenderUtils.drawRect(45F, 14F, 45F + barLength, 18F, getColor(BlendUtils.getHealthColor(entity.health, entity.maxHealth)).rgb)
 
         for (i in 0..9)
             RenderUtils.drawRectBasedBorder(45F + i * 7F, 14F, 45F + (i + 1) * 7F, 18F, 0.5F, getColor(Color.black).rgb)
 
-        Fonts.fontVerdana.drawString("HP: ${entity.health.toInt()} | Dist: ${mc.thePlayer.getDistanceToEntityBox(entity).toInt()}", 46F, 19.5F, getColor(-1).rgb)
+        Fonts.fontVerdana.drawString("HP:${entity.health.toInt()} | Dist:${mc.thePlayer.getDistanceToEntityBox(entity).toInt()}", 45F, 19.5F, getColor(-1).rgb)
 
         GlStateManager.resetColor()
         GL11.glPushMatrix()
@@ -51,7 +51,6 @@ class Exhibition(inst: Target): TargetStyle("Exhibition", inst, false) {
         GlStateManager.enableBlend()
         GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0)
         RenderHelper.enableGUIStandardItemLighting()
-        
 
         val renderItem = mc.renderItem
 
@@ -67,7 +66,7 @@ class Exhibition(inst: Target): TargetStyle("Exhibition", inst, false) {
             renderItem.renderItemIntoGUI(stack, x, y)
             renderItem.renderItemOverlays(mc.fontRendererObj, stack, x, y)
 
-            x += 18
+            x += 16
         }
 
         val mainStack = entity.heldItem
