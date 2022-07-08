128cabee4e541049fc6ed42b3d5eceba4916fd78
exit-scammed <bachnguyenkim2007@gmail.com>
2022-07-08 20:57:18 +0700

    k

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/InvManager.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/player/InvManager.kt
@@ -5,6 +5,7 @@
  */
 package net.ccbluex.liquidbounce.features.module.modules.player
 
+import net.ccbluex.liquidbounce.LiquidBounce
 import net.ccbluex.liquidbounce.event.*
 import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
