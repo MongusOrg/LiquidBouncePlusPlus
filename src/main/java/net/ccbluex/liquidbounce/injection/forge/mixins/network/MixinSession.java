4721b08bb38bc66be4f1d4dfe6ad3f42c1c52cfa
exit-scammed <bachnguyenkim2007@gmail.com>
2022-07-09 09:55:48 +0700

    wait what

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/network/MixinSession.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/network/MixinSession.java
@@ -10,10 +10,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Session.class)
 public class MixinSession {
