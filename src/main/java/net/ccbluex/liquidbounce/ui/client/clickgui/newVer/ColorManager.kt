bc33da1652a8f95f469d821d85e7140e487cf208
exit-scammed <bachnguyenkim2007@gmail.com>
2022-07-18 09:49:10 +0700

    what is going on

--- src/main/java/net/ccbluex/liquidbounce/ui/client/clickgui/newVer/ColorManager.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/clickgui/newVer/ColorManager.kt
@@ -3,13 +3,13 @@ package net.ccbluex.liquidbounce.ui.client.clickgui.newVer
 import java.awt.Color
 
 object ColorManager {
-    const val background = Color(32, 32, 32)
-    const val textBox = Color(31, 31, 31)
-    const val dropDown = Color(45, 45, 45)
-    const val button = Color(52, 52, 52)
-    const val moduleBackground = Color(39, 39, 39)
-    const val unusedSlider = Color(154, 154, 154)
-    const val sliderButton = Color(69, 69, 69)
-    const val border = Color(25, 25, 25)
-    const val buttonOutline = Color(59, 59, 59)
+    var background = Color(32, 32, 32)
+    var textBox = Color(31, 31, 31)
+    var dropDown = Color(45, 45, 45)
+    var button = Color(52, 52, 52)
+    var moduleBackground = Color(39, 39, 39)
+    var unusedSlider = Color(154, 154, 154)
+    var sliderButton = Color(69, 69, 69)
+    var border = Color(25, 25, 25)
+    var buttonOutline = Color(59, 59, 59)
 }
--- src/main/java/net/ccbluex/liquidbounce/ui/client/clickgui/newVer/IconManager.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/clickgui/newVer/IconManager.kt
@@ -3,13 +3,13 @@ package net.ccbluex.liquidbounce.ui.client.clickgui.newVer
 import net.minecraft.util.ResourceLocation
 
 object IconManager {
-    const val removeIcon = ResourceLocation("liquidbounce+/notification/new/error.png")
-    const val add = ResourceLocation("liquidbounce+/clickgui/import.png")
-    const val back = ResourceLocation("liquidbounce+/clickgui/back.png")
-    const val docs = ResourceLocation("liquidbounce+/clickgui/docs.png")
-    const val download = ResourceLocation("liquidbounce+/clickgui/download.png")
-    const val folder = ResourceLocation("liquidbounce+/clickgui/folder.png")
-    const val online = ResourceLocation("liquidbounce+/clickgui/online.png")
-    const val reload = ResourceLocation("liquidbounce+/clickgui/reload.png")
-    const val search = ResourceLocation("liquidbounce+/clickgui/search.png")
+    var removeIcon = ResourceLocation("liquidbounce+/notification/new/error.png")
+    var add = ResourceLocation("liquidbounce+/clickgui/import.png")
+    var back = ResourceLocation("liquidbounce+/clickgui/back.png")
+    var docs = ResourceLocation("liquidbounce+/clickgui/docs.png")
+    var download = ResourceLocation("liquidbounce+/clickgui/download.png")
+    var folder = ResourceLocation("liquidbounce+/clickgui/folder.png")
+    var online = ResourceLocation("liquidbounce+/clickgui/online.png")
+    var reload = ResourceLocation("liquidbounce+/clickgui/reload.png")
+    var search = ResourceLocation("liquidbounce+/clickgui/search.png")
 }
