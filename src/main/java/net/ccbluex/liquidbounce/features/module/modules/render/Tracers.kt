889d9ebfdef383f805947b4e60439922e4b10843
exit-scammed <bachnguyenkim2007@gmail.com>
2022-07-26 13:39:23 +0700

    or maybe not.

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Tracers.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/render/Tracers.kt
@@ -11,6 +11,7 @@ import net.ccbluex.liquidbounce.features.module.Module
 import net.ccbluex.liquidbounce.features.module.ModuleCategory
 import net.ccbluex.liquidbounce.features.module.ModuleInfo
 import net.ccbluex.liquidbounce.utils.EntityUtils
+import net.ccbluex.liquidbounce.utils.RotationUtils
 import net.ccbluex.liquidbounce.utils.render.ColorUtils
 import net.ccbluex.liquidbounce.utils.render.RenderUtils
 import net.ccbluex.liquidbounce.value.BoolValue
