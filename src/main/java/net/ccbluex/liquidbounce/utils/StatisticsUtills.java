ed201fe05861e4a5bce87c8da4ef31cf39993953
AmoSense <109676585+AmoSense@users.noreply.github.com>
2022-07-29 00:17:04 +0700

    help.

---
+++ src/main/java/net/ccbluex/liquidbounce/utils/StatisticsUtills.java
@@ -0,0 +1,35 @@
+package net.ccbluex.liquidbounce.utils;
+
+import net.ccbluex.liquidbounce.event.EntityKilledEvent;
+import net.ccbluex.liquidbounce.event.EventTarget;
+import net.ccbluex.liquidbounce.event.Listenable;
+import net.minecraft.entity.player.EntityPlayer;
+
+public class StatisticsUtils implements Listenable {
+    private static int kills;
+    private static int won;
+
+    @EventTarget
+    public void onTargetKilled(EntityKilledEvent event) {
+        if (!(event.getTargetEntity() instanceof EntityPlayer)) {
+            return;
+        }
+
+        kills++;
+    }
+
+    public static void addWons() {
+        won++;
+    }
+
+    public static int getWons() {
+        return won;
+    }
+
+    public static int getKills() {
+        return kills;
+    }
+
+    @Override
+    public boolean handleEvents() { return true; }
+}
