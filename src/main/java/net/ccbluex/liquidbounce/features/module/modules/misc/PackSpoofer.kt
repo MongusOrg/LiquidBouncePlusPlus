d1b258dc6c7c8ee35da15273eb9d35daaf3bb35a
AmoSense <109676585+AmoSense@users.noreply.github.com>
2022-07-28 23:20:20 +0700

    xd

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/PackSpoofer.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/PackSpoofer.kt
@@ -44,8 +44,9 @@ class PackSpoofer : Module() {
 
                     if (!file2.isFile() || url.contains("liquidbounce", true)) {
                         if (antiExploit.state && antiExploit.notifyValue.get()) {
-                            ClientUtils.displayChatMessage("§8[§9§lLiquidBounce+§8] §6Resourcepack exploit detected.")
-                            ClientUtils.displayChatMessage("§8[§9§lLiquidBounce+§8] §7Exploit target directory: §r$url")
+                            ClientUtils.displayChatMessage("§8[§9§lLiquidBounce++§8] §6Resourcepack directory exploit detected.")
+                            ClientUtils.displayChatMessage("§8[§9§lLiquidBounce++§8] §7Exploit target directory: §r$url")
+                            ClientUtils.displayChatMessage("§8[§9§lLiquidBounce++§8] §9LiquidBounce++ §7Blocked the exploit.")
 
                             throw URISyntaxException(url, "Invalid levelstorage resourcepack path")
                         } else {
