9302d9d65f7c6140267a9e6d276e2716e0b6f5c6
mos <77763580+TheMosKau@users.noreply.github.com>
2022-08-14 13:19:36 +0700

    Update RotationUtils.java

--- src/main/java/net/ccbluex/liquidbounce/utils/RotationUtils.java
+++ src/main/java/net/ccbluex/liquidbounce/utils/RotationUtils.java
@@ -1,4 +1,4 @@
-ll/*
+/*
  * LiquidBounce+ Hacked Client
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
  * https://github.com/WYSI-Foundation/LiquidBouncePlus/
