9b32ef4a561ccbc61e4166aa6b6ced70c33444c2
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2022-09-23 21:13:44 +0100

    Update MixinItemRenderer.java

--- src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/item/MixinItemRenderer.java
+++ src/main/java/net/ccbluex/liquidbounce/injection/forge/mixins/item/MixinItemRenderer.java
@@ -873,17 +873,3 @@ public abstract class MixinItemRenderer {
         }
     }
 }
-Footer
-© 2022 GitHub, Inc.
-Footer navigation
-Terms
-Privacy
-Security
-Status
-Docs
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
