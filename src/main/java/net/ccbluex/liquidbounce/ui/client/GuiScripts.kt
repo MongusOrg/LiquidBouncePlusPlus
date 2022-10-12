143a103995a2145de4a5dc0ea27a7ce31cd1e601
Coccocoa's Helper <68158102+gabrielvicenteYT@users.noreply.github.com>
2022-10-12 22:23:32 +0100

    Update GuiScripts.kt

--- src/main/java/net/ccbluex/liquidbounce/ui/client/GuiScripts.kt
+++ src/main/java/net/ccbluex/liquidbounce/ui/client/GuiScripts.kt
@@ -1,7 +1,7 @@
 /*
- * LiquidBounce+ Hacked Client
+ * LiquidBounce++ Hacked Client
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
- * https://github.com/WYSI-Foundation/LiquidBouncePlus/
+ * https://github.com/PlusPlusMC/LiquidBouncePlusPlus/
  */
 package net.ccbluex.liquidbounce.ui.client
 
