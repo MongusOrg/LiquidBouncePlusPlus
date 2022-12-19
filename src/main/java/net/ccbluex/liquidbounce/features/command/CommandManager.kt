18a15a7f5923ffd1c3b2ea2ae0c63749af7acd02
TheMosKau <rdeefpctest@gmail.com>
2022-12-19 14:11:47 +0700

    i forgor

--- src/main/java/net/ccbluex/liquidbounce/features/command/CommandManager.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/command/CommandManager.kt
@@ -58,6 +58,7 @@ class CommandManager {
         registerCommand(RenameCommand())
         registerCommand(UsernameCommand())
         registerCommand(XrayCommand())
+        registerCommand(SnowFallCommand())
     }
 
     /**
